a74bfb5010a00cbfba224467508aae9d4141e335
enrifeka <enrifeka@yahoo.com>
2016-09-05 11:07:11 +0200

    all tests still not implemented

--- xmlSplitter_test.go
+++ xmlSplitter_test.go
@@ -2,10 +2,18 @@ package main
 
 import "testing"
 
+func TestMain(m *testing.M) {
+
+}
+
 func TestWriteToLogFile(t *testing.T) {
 
 }
 
 func TestGetFirstXMLFileFound(t *testing.T) {
 	t.Error("Test failed")
+}
+
+func TestCreateSplittedXMLFiles(t *testing.T) {
+
 }
